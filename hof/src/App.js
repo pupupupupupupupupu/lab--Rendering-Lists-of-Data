03a89ba0ef3513c72b83594ae369af6372d4a0d7
Arsalan Hassan <114077170+pupupupupupupupupu@users.noreply.github.com>
2023-01-10 12:36:53 +0530

    Add files via upload

---
+++ hof/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import HigherOrderComponent from "./components/HigherOrderComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <HigherOrderComponent></HigherOrderComponent>
+    </div>
+  );
+}
+
+export default App;
